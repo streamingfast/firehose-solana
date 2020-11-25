1eb9c5209e4030c30e32c69d7bd9462fc4175540
billettc <charles@cbillette.com>
2020-11-25 13:45:20 -0500

    added registered vs not registered token distinction in the api

--- graphql/resolvers/query.go
+++ graphql/resolvers/query.go
@@ -6,6 +6,43 @@ import (
 	"github.com/dfuse-io/solana-go"
 )
 
+type TokenRequest struct {
+	Address string
+}
+
+func (r *Root) RegisteredToken(req *TokenRequest) (*RegisteredTokensResponse, error) {
+	pubKey, err := solana.PublicKeyFromBase58(req.Address)
+	if err != nil {
+		return nil, err
+	}
+	t := r.tokenRegistry.GetToken(&pubKey)
+	if t == nil || t.Meta == nil {
+		return nil, nil
+	}
+	return RegisteredTokensResponseFromRegistryEntry(t), nil
+}
+
+func (r *Root) RegisteredTokens() (out []*RegisteredTokensResponse) {
+	out = []*RegisteredTokensResponse{}
+	for _, t := range r.tokenRegistry.GetTokens() {
+		if t.Meta != nil {
+			out = append(out, RegisteredTokensResponseFromRegistryEntry(t))
+		}
+	}
+	return
+}
+func (r *Root) Token(req *TokenRequest) (*TokensResponse, error) {
+	pubKey, err := solana.PublicKeyFromBase58(req.Address)
+	if err != nil {
+		return nil, err
+	}
+	t := r.tokenRegistry.GetToken(&pubKey)
+	if t == nil {
+		return nil, nil
+	}
+	return TokensResponseFromRegistryEntry(t), nil
+}
+
 func (r *Root) Tokens() (out []*TokensResponse) {
 	out = []*TokensResponse{}
 	for _, t := range r.tokenRegistry.GetTokens() {
@@ -14,20 +51,18 @@ func (r *Root) Tokens() (out []*TokensResponse) {
 	return
 }
 
-type SplTokenRequest struct {
-	Address string
-}
-
-func (r *Root) Token(req *SplTokenRequest) (*TokensResponse, error) {
-	pubKey, err := solana.PublicKeyFromBase58(req.Address)
-	if err != nil {
-		return nil, err
-	}
-	t := r.tokenRegistry.GetToken(&pubKey)
-	return TokensResponseFromRegistryEntry(t), nil
-}
-
 type TokensResponse struct {
+	Address         string
+	MintAddress     string
+	MintAuthority   string
+	FreezeAuthority string
+	supply          uint64
+	Decimals        int32
+}
+
+func (t *TokensResponse) Supply() types.Uint64 { return types.Uint64(t.supply) }
+
+type RegisteredTokensResponse struct {
 	Address         string
 	MintAddress     string
 	MintAuthority   string
@@ -39,11 +74,11 @@ type TokensResponse struct {
 	Logo            string
 }
 
-func (t *TokensResponse) Supply() types.Uint64 { return types.Uint64(t.supply) }
+func (t *RegisteredTokensResponse) Supply() types.Uint64 { return types.Uint64(t.supply) }
 
-func TokensResponseFromRegistryEntry(token *token.RegisteredToken) *TokensResponse {
+func RegisteredTokensResponseFromRegistryEntry(token *token.RegisteredToken) *RegisteredTokensResponse {
 
-	r := &TokensResponse{
+	r := &RegisteredTokensResponse{
 		Address:         token.Address.String(),
 		MintAuthority:   token.MintAuthority.String(),
 		FreezeAuthority: token.FreezeAuthority.String(),
@@ -54,6 +89,17 @@ func TokensResponseFromRegistryEntry(token *token.RegisteredToken) *TokensRespon
 		r.Symbol = token.Meta.Symbol
 		r.Name = token.Meta.Name
 		r.Logo = token.Meta.Logo
+	}
+	return r
+}
+func TokensResponseFromRegistryEntry(token *token.RegisteredToken) *TokensResponse {
+
+	r := &TokensResponse{
+		Address:         token.Address.String(),
+		MintAuthority:   token.MintAuthority.String(),
+		FreezeAuthority: token.FreezeAuthority.String(),
+		supply:          uint64(token.Supply),
+		Decimals:        int32(token.Decimals),
 	}
 	return r
 }
