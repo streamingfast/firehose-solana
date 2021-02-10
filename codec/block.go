c4a44d676abedb7a01bc1b147c75b8f9a9ea8b9e
billettc <charles@cbillette.com>
2021-02-10 11:35:23 -0500

    undo -> sol block LIBNum is now root block num -100

--- codec/block.go
+++ codec/block.go
@@ -21,7 +21,7 @@ func BlockFromProto(slot *pbcodec.Slot) (*bstream.Block, error) {
 		Number:         slot.Num(),
 		PreviousId:     slot.PreviousId,
 		Timestamp:      blockTime,
-		LibNum:         slot.LIBNum() - 100,
+		LibNum:         slot.LIBNum(),
 		PayloadKind:    Protocol_SOL,
 		PayloadVersion: int32(slot.Version),
 		PayloadBuffer:  content,
