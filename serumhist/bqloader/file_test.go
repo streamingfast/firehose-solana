5255d446831483ddfe1d891e73c9077faf427e38
colindickson <colin.dickson@gmail.com>
2021-02-17 14:34:07 -0500

    WIP: bq handler, simplify tests

--- serumhist/bqloader/file_test.go
+++ serumhist/bqloader/file_test.go
@@ -6,21 +6,21 @@ import (
 )
 
 func Test_FileNameString(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    *FileName
-		expected string
-	}{
-		{
-			name:     "basic",
-			input:    NewFileName("testPrefix", 0, 100, "a", "b", "2020-01-01-12345"),
-			expected: "testPrefix/0-100-a-b-2020-01-01-12345.avro",
-		},
-	}
+	input := NewFileName("testPrefix", 0, 100, "a", "b", "2020-01-01-12345")
+	expected := "testPrefix/0-100-a-b-2020-01-01-12345.avro"
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			assert.Equal(t, test.expected, test.input.String())
-		})
-	}
+	assert.Equal(t, expected, input.String())
+
+}
+
+func Test_parseLatestInfoFromFilename(t *testing.T) {
+	input := "testPrefix/0-100-a-b-2020-01-01-12345.avro"
+	expected := &FileName{
+	LatestSlotNum: 100,
+	LatestSlotId:  "b",
+}
+
+	output, err := parseLatestInfoFromFilename(input)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, output)
 }
