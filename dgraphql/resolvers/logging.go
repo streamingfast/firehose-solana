d792eac5b3f1215459a6a8b8422eea5f0589d229
Julien Cassis <juliencassis@gmail.com>
2022-09-26 14:49:37 -0400

    remove dgraphql application

--- dgraphql/resolvers/logging.go
+++
@@ -1,7 +0,0 @@
-package resolvers
-
-import (
-	"github.com/streamingfast/logging"
-)
-
-var zlog, tracer = logging.PackageLogger("resolvers", "github.com/streamingfast/firehose-solana/dgraphql/resolvers")
