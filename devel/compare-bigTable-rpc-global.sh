a055d2fd1a5e86c5b98d959af8a89f8910858f00
arnaudberger <arnaudberger84@gmail.com>
2024-01-25 09:44:33 -0500

    Add environmental variables within the global compare bash script

--- devel/compare-bigTable-rpc-global.sh
+++ devel/compare-bigTable-rpc-global.sh
@@ -6,16 +6,25 @@ runningTimes=10
 current_block=1690600
 eachNumberOfBlocks=10000000
 stop_block=240000000
-rpc_endpoint="https://frequent-sparkling-hill.solana-mainnet.quiknode.pro/ff194d392c35948e3ef04003d141cda78dbf9733/"
-reference_storage="gs://dfuseio-global-blocks-uscentral/sol-mainnet/v1?project=dfuseio-global"
-current_storage="file:///Users/arnaudberger/t/data/storage/merged-blocks"
+
+source ../.envrc
+
+rpc_endpoint="$RPC_ENDPOINT"
+reference_storage="$REFERENCE_STORAGE"
+current_storage="$CURRENT_STORAGE"
+
+# Now use the variables directly
+echo "RPC Endpoint: ${rpc_endpoint}"
+echo "Reference Storage: $REFERENCE_STORAGE"
+echo "Current Storage: $CURRENT_STORAGE"
+
 function fetch_rpc_blocks_for_range() {
   local rpc_endpoint=$1
   local start_block=$2
   local stop_block=$3
   rm -rf ~/t/data/
 
-  firecore start reader-node merger -c ""  --merger-stop-block ${stop_block} --data-dir=/Users/arnaudberger/t/data --reader-node-data-dir=/Users/arnaudberger/t/data --reader-node-path=firesol --reader-node-arguments="fetch rpc ${rpc_endpoint} ${start_block} --state-dir /Users/arnaudberger/t/data" --common-first-streamable-block=${start_block}
+  firecore start reader-node merger -c ""  --merger-stop-block ${stop_block} --data-dir=/Users/arnaudberger/t/data --reader-node-data-dir=/Users/arnaudberger/t/data --reader-node-path=firesol --reader-node-arguments="fetch rpc "${rpc_endpoint}" ${start_block} --state-dir /Users/arnaudberger/t/data" --common-first-streamable-block=${start_block}
 }
 
 function compare_reference_rpc_for_range() {
